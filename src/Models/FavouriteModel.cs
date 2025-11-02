7022d31e0fbff95d37b858dc2431f91df5fec40a
Nico-Fer <fernandeznicolas1801@gmail.com>
2025-11-02 16:00:35 -0300

    refactor clase FavouriteModel

--- src/Models/FavouriteModel.cs
+++ src/Models/FavouriteModel.cs
@@ -1,8 +1,24 @@
-﻿namespace StyleMatch.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StyleMatch.Models
 {
+    /// <summary>
+    /// Entidad para la representación de un grupo de favoritos
+    /// </summary>
     public class FavouriteModel
     {
+        /// <summary>
+        /// Identificador externo del grupo de favoritos
+        /// </summary>
         public Guid? ExternalId { get; set; }
+        /// <summary>
+        /// Nombre del grupo de favoritos
+        /// </summary>
+        [Required]
         public string Name { get; set; }
+        /// <summary>
+        /// Prendas
+        /// </summary>
+        public IEnumerable<GarmentModel>? Garments { get; set; }
     }
 }
